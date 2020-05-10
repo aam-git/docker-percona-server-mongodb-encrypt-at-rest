b512fcf2442fb8cfd4d1a8dc6b2a0e3d1d94f360
Aaron Moy <info@aamservices.uk>
2020-05-10 14:09:32 +0100

    remove new line in base encoded string

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ fi
     if [ "$1" = 'mongod' ]; then
     
       if [ ! -f /data/key/mongodb.key ]; then
-		cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 32 | head -n 1 | tr '' '\n' | base64 | xargs printf '%s' > /data/key/mongodb.key
+		cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 32 | head -n 1 | xargs printf '%s' | base64 | xargs printf '%s' > /data/key/mongodb.key
         chmod 600 /data/key/mongodb.key
       fi
 
