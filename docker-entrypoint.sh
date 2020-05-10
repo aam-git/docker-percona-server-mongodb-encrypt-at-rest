04f6eab841f56284eebca06ed08b191723638009
AaronM <info@aamservices.uk>
2020-05-10 10:27:34 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,17 +3,18 @@ set -e
 FILE = /data/key/mongodb.key
 
 if [ "${1:0:1}" = '-' ]; then
-	set -- mongod "$@"
-
-if [ "$1" = 'mongod' ]; then
-
-    if [ !-f "$FILE" ]; then
-        mkdir /data/key
-		openssl rand -base64 32 > /data/key/mongodb.key
-		chmod 600 /data/key/mongodb.key
-    fi
-
-    exec "$@"
+  set -- mongod "$@"
 fi
+
+    if [ "$1" = 'mongod' ]; then
+
+      if [ !-f "$FILE" ]; then
+        mkdir /data/key
+        openssl rand -base64 32 > /data/key/mongodb.key
+        chmod 600 /data/key/mongodb.key
+      fi
+
+      exec "$@"
+    fi
 
 exec "$@"
