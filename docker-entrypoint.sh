bff3ee2a58c0266d4f6c2353d3e443f4231c5a43
Aaron Moy <info@aamservices.uk>
2020-05-10 16:11:09 +0100

    move the key init again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -f /data/key/mongodb.key ]; then
+	cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 32 | head -n 1 | xargs printf '%s' | base64 | xargs printf '%s' > /data/key/mongodb.key
+	chmod 600 /data/key/mongodb.key
+fi
+
 set -Eeuo pipefail
 
 if [ "${1:0:1}" = '-' ]; then
@@ -449,15 +454,5 @@ fi
 rm -f "$jsonConfigFile" "$tempConfigFile"
 
 set -o xtrace
-
-    if [ "$1" = 'mongod' ]; then
-    
-      if [ ! -f /data/key/mongodb.key ]; then
-		cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 32 | head -n 1 | xargs printf '%s' | base64 | xargs printf '%s' > /data/key/mongodb.key
-        chmod 600 /data/key/mongodb.key
-      fi
-
-      exec "$@"
-    fi
 
 exec "$@"
